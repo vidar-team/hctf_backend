f184e3363754ee8bc40bec96a001650505cadd98
iAklis <iAklis@users.noreply.github.com>
2017-08-21 20:24:07 +0800

    Add teams table

---
+++ database/migrations/2017_08_21_120744_create_team_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('teamName'); // Display Name
+            $table->string('email')->unique(); // Login Name
+            $table->string('password');
+            $table->dateTimeTz('signUpTime');
+            $table->dateTimeTz('lastLoginTime');
+            $table->decimal('score');
+            $table->boolean('banned');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
