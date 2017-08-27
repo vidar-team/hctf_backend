c8a48c0475905ab9f05543228816e9ff70c775f5
iAklis <iAklis@users.noreply.github.com>
2017-08-27 12:57:49 +0800

    Sora wannt to get the json result of exception

--- app/Http/Middleware/VerifyJWTToken.php
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -21,19 +21,23 @@ class VerifyJWTToken
     public function handle($request, Closure $next)
     {
         if (! $token = $this->auth->setRequest($request)->getToken()) {
-            return $this->respond('tymon.jwt.absent', 'token_not_provided', 400);
+//            return $this->respond('tymon.jwt.absent', 'token_not_provided', 400);
+            return $this->respond()->json(['token_not_provided'], 400);
         }
 
         try {
             $user = $this->auth->authenticate($token);
         } catch (TokenExpiredException $e) {
-            return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+//            return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+            return $this->respond()->json(['token_expired'], $e->getStatusCode());
         } catch (JWTException $e) {
-            return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+//            return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+            return $this->respond()->json(['token_invalid'], $e->getStatusCode());
         }
 
         if (! $user) {
-            return $this->respond('tymon.jwt.user_not_found', 'user_not_found', 404);
+//            return $this->respond('tymon.jwt.user_not_found', 'user_not_found', 404);
+            return $this->respond()->json(['user_not_found'], 404);
         }
 
         $this->events->fire('tymon.jwt.valid', $user);
