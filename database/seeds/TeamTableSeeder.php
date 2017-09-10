46ce9244b7c96d94ea44990aee03b73eee9f7999
iAklis <iAklis@users.noreply.github.com>
2017-09-10 20:43:56 +0800

    Fix Controller For the Modification of Model

--- database/seeds/TeamTableSeeder.php
+++ database/seeds/TeamTableSeeder.php
@@ -13,12 +13,33 @@ class TeamTableSeeder extends Seeder
      */
     public function run()
     {
-        $team = new Team;
-        $team->teamName = 'Vidar';
-        $team->email = 'aklis@vidar.club';
-        $team->password = bcrypt(hash('sha256', 'aklis@hctf'));
-        $team->signUpTime = Carbon::now('Asia/Shanghai');
-        $team->lastLoginTime = Carbon::now('Asia/Shanghai');
-        $team->save();
+//        $team = new Team;
+//        $team->team_name = 'Vidar';
+//        $team->email = 'aklis@vidar.club';
+//        $team->password = bcrypt(hash('sha256', 'aklis@hctf'));
+//        $team->signUpTime = Carbon::now('Asia/Shanghai');
+//        $team->lastLoginTime = Carbon::now('Asia/Shanghai');
+//        $team->save();
+
+        DB::table('teams')->delete();
+
+        $teams = [
+            array(
+                'team_name' => 'Vidar',
+                'email' => 'aklis@vidar.club',
+                'password' => bcrypt(hash('sha256', 'aklis@hctf')),
+                'signUpTime' => Carbon::now('Asia/Shanghai'),
+                'lastLoginTime' => Carbon::now('Asia/Shanghai'),
+            ),
+            array(
+                'team_name' => 'HDUISA',
+                'email' => 'birdway@vidar.club',
+                'password' => bcrypt(hash('sha256', 'aklis@hctf')),
+                'signUpTime' => Carbon::now('Asia/Shanghai'),
+                'lastLoginTime' => Carbon::now('Asia/Shanghai'),
+            ),
+        ];
+
+        DB::table('teams')->insert($teams);
     }
 }
