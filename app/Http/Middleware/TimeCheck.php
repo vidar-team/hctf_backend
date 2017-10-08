1f133321a0adf38be41379990edcd830b4fcefd8
Eridanus Sora <haolinaihui@Gmail.com>
2017-10-08 16:47:27 +0800

    fix seeder

---
+++ app/Http/Middleware/TimeCheck.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Http\Middleware;
+
+class TimeCheck {
+    public function handle($request){
+
+    }
+}
--- database/seeds/TeamTableSeeder.php
+++ database/seeds/TeamTableSeeder.php
@@ -13,32 +13,27 @@ class TeamTableSeeder extends Seeder
      */
     public function run()
     {
-//        $team = new Team;
-//        $team->team_name = 'Vidar';
-//        $team->email = 'aklis@vidar.club';
-//        $team->password = bcrypt(hash('sha256', 'aklis@hctf'));
-//        $team->signUpTime = Carbon::now('Asia/Shanghai');
-//        $team->lastLoginTime = Carbon::now('Asia/Shanghai');
-//        $team->save();
-
         DB::table('teams')->delete();
 
         $teams = [
-            array(
+            [
                 'team_name' => 'Vidar',
                 'email' => 'aklis@vidar.club',
                 'password' => bcrypt(hash('sha256', 'aklis@hctf')),
                 'signUpTime' => Carbon::now('Asia/Shanghai'),
                 'admin' => true,
+                'banned' => false,
                 'lastLoginTime' => Carbon::now('Asia/Shanghai'),
-            ),
-            array(
+            ],
+            [
                 'team_name' => 'HDUISA',
                 'email' => 'birdway@vidar.club',
                 'password' => bcrypt(hash('sha256', 'aklis@hctf')),
                 'signUpTime' => Carbon::now('Asia/Shanghai'),
+                'admin' => false,
+                'banned' => false,
                 'lastLoginTime' => Carbon::now('Asia/Shanghai'),
-            ),
+            ],
         ];
 
         DB::table('teams')->insert($teams);
