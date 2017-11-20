cb8f6e5b1c4993f626345e5489382adf97474256
Eridanus Sora <haolinaihui@Gmail.com>
2017-11-20 22:13:42 +0800

    support base score

--- app/Services/ScoreService.php
+++ app/Services/ScoreService.php
@@ -2,13 +2,13 @@
 namespace App\Services;
 class ScoreService
 {
-    static function calculate($solvedCount){
+    static function calculate($solvedCount, $baseScore = 1000, $minimumScore = 10){
         if ($solvedCount === 1){
-            return 1000;
+            return $baseScore;
         }
-        $score = -28730*pow($solvedCount, 0.007236) + 29840;
-        if ($score <= 10){
-            return 10;
+        $score = ($baseScore / 1000) * (-28730 * pow($solvedCount, 0.007236) + 29840);
+        if ($score <= $minimumScore){
+            return $minimumScore;
         }
         return round($score, 2);
     }
